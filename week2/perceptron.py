7c498e178ae94276e1ed74cb5bfc059497cb8e09
sunyeon-Jeong <sunyeon4012@gmail.com>
2024-10-03 17:45:07 +0900

    Feat : week2(퍼셉트론 scikit-learn perceptron) #2 - confusion_matrix_of_digit_classification - predicted_digits_from_testset

--- week2/perceptron.py
+++ week2/perceptron.py
@@ -46,3 +46,44 @@ for i in range(10): # 필기체 0~9
     ax.imshow(X[i].reshape(8, 8)) # 64차원 벡터 -> 8*8 이미지 형태로 변형
     ax.set_title(str(i)) # 서브플롯 타이틀을 인덱스 값으로 
 plt.show()
+
+# 학습/테스트 데이터 분리
+from sklearn.model_selection import train_test_split
+X, y = load_digits(return_X_y=True) # X = 데이터, y = 레이블
+# 25% -> 테스트, 75% -> 훈련용
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=1)
+
+# 퍼셉트론 모델 객체 생성
+# - tol(tolerance) : 허용오차. 모델가중치(w)가 tol 값보다 작을 경우 학습 종료 (더이상 개선 가능성X)
+clf = Perceptron(tol=1e-3, random_state=0) # 1e-3 : 0.001
+
+# 퍼셉트론 모델 학습
+clf.fit(X_train, y_train)
+
+# 테스트 데이터 결과 예측
+y_pred = clf.predict(X_test)
+
+from sklearn.metrics import classification_report
+
+target_names = ['digit 0', 'digit 1', 'digit 2', 'digit 3', 'digit 4',
+                'digit 5', 'digit 6', 'digit 7', 'digit 8', 'digit 9']
+
+# 참값(y_test)과 예측값(y_pred)을 대상으로 classfication_report 생성
+print(classification_report(y_test, y_pred, target_names=target_names))
+
+from sklearn.metrics import ConfusionMatrixDisplay
+
+# 참값(y_test)과 예측값(y_pred)을 대상으로 confusion matrix 생성
+ConfusionMatrixDisplay.from_predictions(y_test, y_pred)
+
+rows = 2; cols = 5
+fig = plt.figure()
+
+fig.tight_layout(w_pad=100)
+
+for i in range(10):
+    ax = fig.add_subplot(rows, cols, i+1)
+    ax.imshow(X_test[i].reshape(8, 8))
+    ax.set_title("y_hat = " + str(y_pred[i]))
+
+plt.show()
