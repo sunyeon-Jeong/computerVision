1ebfdb33ce0ed997e76c7f5425aafe6b00966a34
sunyeon-Jeong <sunyeon4012@gmail.com>
2024-10-03 16:15:59 +0900

    Feat : week2(퍼셉트론 scikit-learn perceptron) #2 - 라이브러리 import - digits dataset 로딩

--- week2/perceptron.py
+++ week2/perceptron.py
@@ -29,3 +29,20 @@
   : 학습모델의 분류성능을 시각적으로 확인
   : 목표클래스 성공/실패 수를 알려줌 -> 모델의 분류 성능을 한눈에 파악 가능
 '''
+
+# 라이브러리 import
+from sklearn.datasets import load_digits # 학습 데이터
+from sklearn.linear_model import Perceptron # 퍼셉트론 모델
+from matplotlib import pyplot as plt # 데이터 시각화 라이브러리
+
+# 학습데이터 로딩 -> X(특징벡터), y(클래스 레이블)로 구분
+X, y = load_digits(return_X_y=True)
+
+rows = 2; cols = 5 # 플롯 행/열 선언
+fig = plt.figure() # 새로운 플롯 객체 생성
+
+for i in range(10): # 필기체 0~9
+    ax = fig.add_subplot(rows, cols, i+1) # 2행 5열 플롯에서 인덱스에 해당하는 새로운 서브플롯 생성
+    ax.imshow(X[i].reshape(8, 8)) # 64차원 벡터 -> 8*8 이미지 형태로 변형
+    ax.set_title(str(i)) # 서브플롯 타이틀을 인덱스 값으로 
+plt.show()
